9531ef61ffed95bdb1563297aea860f4b3c2c0fc
Markus Sommerfeld <msommerfeld@silversurfer7.de>
2019-03-21 18:26:31 +0100

    renamed to php

--- Docker/app/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "; Additional xdebug settings" >> $PHP_CONF_DIR/xdebug.ini \
-    && echo xdebug.remote_handler=1                           >> $PHP_CONF_DIR/xdebug.ini \
-    && echo xdebug.remote_handler=$XDEBUG_REMOTE_HANDLER    >> $PHP_CONF_DIR/xdebug.ini \
-    && echo xdebug.remote_port=$XDEBUG_REMOTE_PORT          >> $PHP_CONF_DIR/xdebug.ini \
-    && echo xdebug.remote_host=$XDEBUG_REMOTE_HOST          >> $PHP_CONF_DIR/xdebug.ini
-
-cat $PHP_CONF_DIR/xdebug.ini
