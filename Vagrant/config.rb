4a3d41d64a737be3f9974c0e277703b6418499e6
Markus <msommerfeld@silversurfer7.de>
2019-03-29 10:25:44 +0100

    new config parameter added to run the vagrant-up trigger with additional arguments

--- Vagrant/config.rb
+++ Vagrant/config.rb
@@ -4,7 +4,10 @@
 $VM_BOX = 'generic/debian9'
 
 # Starts the Docker deamon on vagrant up
-$AUTOSTART_DOCKER = true
+$AUTOSTART_DOCKER   = true
+
+# Rebuilds the docker images
+$DOCKER_ARGUMENTS   = "--build"
 
 # Provider-specific configuration so you can fine-tune various
 # backing providers for Vagrant. These expose provider-specific options.
@@ -17,6 +20,8 @@ $VM_NAME            = 'dev_env_docker'
 $VM_HOST_NAME       = 'dev.docker'
 # The VM Host Aliases
 $VM_HOST_ALIASES    = {
+    symfony:    'symfony.docker',
+    yii:        'yii.docker'
 }
 
 # Display the VirtualBox GUI when booting the machine
