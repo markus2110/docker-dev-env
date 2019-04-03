552c5f22ce389181f6fa91db347ae2b0c34a5427
Markus Sommerfeld <msommerfeld@silversurfer7.de>
2019-04-03 13:35:48 +0200

    header added

--- Application/Base/incl/send-mail.inc.php
+++ Application/Base/incl/send-mail.inc.php
@@ -1,8 +1,12 @@
 <?php
 
+$header = 'From: webmaster@dev.docker' . "\r\n" .
+    'Reply-To: webmaster@dev.docker' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
    echo "<h3>SEND MAIL Testing</h3>";
    try{
-       $r = mail('mail@example.docker', 'The Mail Subject', "The mail text");
+       $r = mail('mail@dev.docker', 'The Mail Subject', "The mail text", $header);
        var_dump($r);
 
        echo "<a href='".sprintf("http://%s:%d", $_SERVER['HTTP_HOST'], 1080)."' target='_blank'>Mailcatcher</a>";
